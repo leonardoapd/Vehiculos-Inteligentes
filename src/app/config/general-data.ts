a9f09c41070a05057d7c97084ee70a813dd2728e
Leonardo Perdomo <leonardop763@gmail.com>
2021-12-13 18:29:38 -0500

    Se borra codigo del recaptcha

---
+++ src/app/config/general-data.ts
@@ -0,0 +1,8 @@
+export namespace GeneralData{
+  export const USERS_URL = 'http://localhost:3000'
+  export const CODE_CAPTCHA = '';
+  export const API_DPTO = 'https://raw.githubusercontent.com/marcovega/colombia-json/master/colombia.json';
+  export const CONTENIDO_NOTIFICACION = ""
+}
+
+
